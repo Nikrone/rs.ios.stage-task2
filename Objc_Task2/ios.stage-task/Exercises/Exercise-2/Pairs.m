18808e21fde2052a14dd31a6aee5a9ed82ac875a
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-05-16 21:29:36 +0300

    feat: exercise-2 implemented

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,18 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    
+    NSInteger count = 0;
+    NSInteger lenght = array.count;
+    for (int i = 0; i < lenght; i++) {
+        for (int j = i+1; j < lenght; j++) {
+            if (abs([array[i] intValue] - [array[j] intValue]) == [number intValue]) {
+                count++;
+            }
+        }
+    }
+    
+    return count;
 }
 
 @end
