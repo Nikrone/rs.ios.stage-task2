cd66e552d78361224e4e3a925cafe59878887f44
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-05-16 21:02:35 +0300

    feat: add implementation for exercise-1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,18 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    
+    UInt8 reversN = 0;
+    
+    for (UInt8 i = 7; i >= 0; i--) {
+        reversN += n % 2 * pow(2, i);
+        n /= 2;
+        if (n < 2) {
+            i--;
+            reversN += n * pow(2, i);
+            break;
+        }
+    }
+    
+    return reversN;
 }
