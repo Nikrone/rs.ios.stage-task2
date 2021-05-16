86060ed0289c3272eefefaf357c22c0b8e1b285b
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-05-16 22:33:12 +0300

    feat: exercise-3, 4 implemented

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,35 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    if (numbersArray == nil || numbersArray.count == 0 ) {
+        return @"";
+    }
+    
+    for (id i in numbersArray) {
+        if ([i intValue] < 0) {
+            return @"Negative numbers are not valid for input.";
+        }
+        if ([i intValue] > 255) {
+            return @"The numbers in the input array can be in the range from 0 to 255.";
+        }
+    }
+    
+    
+    NSMutableString *result = [[NSMutableString alloc] init];
+    
+    for (int v = 0; v < 4; v ++) {
+        if (v < numbersArray.count ) {
+            [result appendFormat:@"%@", [[numbersArray objectAtIndex:v] stringValue]];
+        } else {
+            [result appendString:@"0"];
+        }
+        if (v < 3) {
+            [result appendString:@"."];
+        }
+    }
+    
+    return result;
 }
 
 @end
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,56 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+        NSMutableArray *numArray = [NSMutableArray new];
+        NSMutableArray *strArray = [NSMutableArray new];
+        
+        
+        for (NSObject* subArr in array) {
+            if ([subArr isKindOfClass: NSArray.class]) {
+                for (NSObject* obj in (NSArray*)subArr)
+                {
+                    if ([obj isKindOfClass: NSNumber.class]) {
+                        [numArray addObject:obj];
+                        continue;
+                    }
+                    
+                    if ([obj isKindOfClass: NSString.class]) {
+                        [strArray addObject:obj];
+                    }
+                }
+            }
+            else return @[];
+        }
+        
+        if (numArray.count == 0) {
+            [strArray sortUsingComparator:^NSComparisonResult(id a, id b) {
+                if ([(NSString*)a compare:(NSString*)b] == NSOrderedAscending)
+                    return NSOrderedAscending;
+                else return NSOrderedDescending;
+            }];
+            return strArray;
+        }
+        else
+            [strArray sortUsingComparator:^NSComparisonResult(id a, id b) {
+            if ([(NSString*)a compare:(NSString*)b] == NSOrderedDescending)
+                return NSOrderedAscending;
+            else return NSOrderedDescending;
+        }];
+        
+        
+        [numArray sortUsingComparator:^NSComparisonResult(id a, id b){
+            if ([(NSNumber*) a compare: (NSNumber*)b] == NSOrderedAscending)
+                return NSOrderedAscending;
+            else return NSOrderedDescending;
+        }];
+        if (strArray.count == 0)
+        {
+            return numArray;
+        }
+        
+        return @[numArray, strArray];
+
 }
 
 @end
